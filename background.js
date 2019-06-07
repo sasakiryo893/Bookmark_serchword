fa46042f4b5c558fd39b589358298c6b18a800e4
ryo_sasaki <r.sasaki.tcm@gmail.com>
2019-06-07 16:23:26 +0900

    これが基盤になりそう

--- background.js
+++ background.js
@@ -1,13 +1,20 @@
-// $(function(){
-//   $('#site_list').append('できません');
-//   // alert("aaaaa");
-//   console.log("aaa");
-// });
-
-// $("#search").click(function(){
-//   console.log("aaaaaaaaaaa");
-// });
+//データベースから登録したサイトを表示
 
 $(function(){
-  $('.aaa').append("aaaaaaa");
+  console.log("aaaa");
+  $('.site').before("aaaa");
+  console.log(chrome.runtime.getURL("list_test.json"));
+
+  //タイトルが長すぎたときに文末を省略
+  $('.site_title').each(function(){
+    let cutFigure = '60';
+    let textLength = $(this).text().length;
+    let textTrim = $(this).text().substr(0,(cutFigure));
+
+    if(cutFigure < textLength) {
+        $(this).html(textTrim + "...").css({visibility:'visible'});
+    } else if(cutFigure >= textLength) {
+        $(this).css({visibility:'visible'});
+    }
+  });
 });
