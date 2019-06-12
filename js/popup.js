0c6c25e6448d588006fc9fb11147a6c4a7609b0b
ryo_sasaki <r.sasaki.tcm@gmail.com>
2019-06-12 18:32:51 +0900

    クリックで登録したサイトに遷移できるように

--- js/popup.js
+++ js/popup.js
@@ -1,28 +1,29 @@
-//追加、検索処理
-
-//検索
 $(function(){
   // $('.site_list').append("ここに保存した検索ワード");
 
+  //検索
   $('.search').on('click',function(){
     alert($('.search_word').val() + "で検索！！！！！！！")
   })
 
+  //追加
   $('.add').on('click',function(){
 
     alert("追加！！！！！！！")
     window.location.href = '/popupWindow.html';
   })
 
+  //mausehoverで色を変える
   $('.site_info').hover(function(){
     $(this).css('background', '#f0f8ff');
   },function(){
     $(this).css('background', '');
   });
 
+  //遷移後のURLを取得し新しいタブで開く
   $('.site_info').on('click',function(){
-    // alert($(this).children('.site_url'));
-    alert(JSON.stringify($(this)));
+    let url = $(this).children('.site_url').text();
+    window.open(url,'_brank');
   });
 
 
