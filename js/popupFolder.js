51c1b165cfdb7631c11355ec3c2af09650226f95
nemu-ism <kerokeromirai@gmail.com>
2019-08-09 16:56:48 +0900

    popupFolder.jsのDAOメソッドをprototypeに記述

--- js/popupFolder.js
+++ js/popupFolder.js
@@ -1,7 +1,7 @@
-$(function(){
+$(function() {
   $('input#input_folder_title').focus();
-  $('#Bt_Regi').on('click',function(){
-    var dao = new Dao
+  $('#Bt_Regi').on('click',function() {
+    var dao = new Dao;
     // console.log($('textarea[name="nanndemoii"]').val());
     var name = $('input#input_folder_title').val().trim();
     dao.add_folder(name, getParam(), function() {
@@ -9,17 +9,14 @@ $(function(){
     });
   });
 
-  // リストに戻る
-  $('#Bt_Cancel').on('click',function(){
+  $('#Bt_Cancel').on('click',function() {
     window.location.href = '/popup.html' + "?folder_id=" + getParam();
   });
 
-  // close
-  $('.close').on('click',function(){
+  $('.close').on('click',function() {
     window.close();
   });
-
-})
+});
 
 function getParam() {
   var url = location.href;
@@ -31,16 +28,17 @@ function getParam() {
   return parent_id;
 }
 
-var Dao = function(){
+var Dao = function() {
   var name = 'localdb';
   var version = '1.0';
   var description = 'Web SQL Database';
   var size = 5 * 1024 * 1024;
-  var db = openDatabase(name, version, description, size);
+  this.db = openDatabase(name, version, description, size);
+}
 
-  // 登録
-  this.add_folder = function(name, parent_id, callback){
-    db.transaction(function(tx){
+Dao.prototype = {
+  add_folder: function(name, parent_id, callback) {
+    this.db.transaction(function(tx) {
       tx.executeSql('insert into folders (name, parent_id) values (?, ?)', [name, parent_id]);
       callback();
     });
