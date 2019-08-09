5859cb46fd4d2d1544ec83db39c5a7b486be26a1
nemu-ism <kerokeromirai@gmail.com>
2019-08-09 16:49:51 +0900

    popupWindow.jsのDAOメソッドをprototypeに記述

--- js/popupWindow.js
+++ js/popupWindow.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function() {
   var dao = new Dao();
   // サイト名、URLを取得
   chrome.tabs.getSelected(null, function(tab) {
@@ -23,22 +23,19 @@ $(function(){
     let input_text = substr(tab.title,30,'…');
 
     $('#input_site').html(input_text);
-
   });
 
-  // 最近の検索ワード20件を取得してプルダウンメニューに挿入
   chrome.history.search({
     text: '- Google 検索',
     maxResults: 20
   },
-  function (results) {
-    for(var i = 0; i < results.length; i++) {
+  function(results) {
+    for (var i = 0; i < results.length; i++) {
       $('#search_word').append($('<option>').html(results[i].title.replace('- Google 検索', '')).val(results[i].title.replace('- Google 検索', '')));
     };
   });
 
-  // 登録
-  $('#Bt_Regi').on('click',function(){
+  $('#Bt_Regi').on('click',function() {
     chrome.tabs.getSelected(null, function(tab){
       var site = tab.title;
       var url = tab.url;
@@ -51,14 +48,12 @@ $(function(){
     });
   });
 
-  // topに戻る
-  $('#Bt_Cancel').on('click',function(){
+  $('#Bt_Cancel').on('click',function() {
   var folder_id = getParam();
     window.location.href = '/popup.html' + "?folder_id=" + folder_id;
   })
 
-  // close
-  $('.close').on('click',function(){
+  $('.close').on('click',function() {
     window.close();
   })
 });
@@ -73,19 +68,19 @@ function getParam() {
   return folder_id;
 }
 
-var Dao = function(){
+var Dao = function() {
   var name = 'localdb';
   var version = '1.0';
   var description = 'Web SQL Database';
   var size = 5 * 1024 * 1024;
-  var db = openDatabase(name, version, description, size);
+  this.db = openDatabase(name, version, description, size);
+}
 
-  // 登録
-  this.add_bookmark = function(site, url, word, memo, folder_id, callback){
-    db.transaction(function (tx){
+Dao.prototype = {
+  add_bookmark: function(site, url, word, memo, folder_id, callback) {
+    this.db.transaction(function(tx) {
       tx.executeSql('insert into bookmarks (name, url, search_word, memo, folder_id) values (?, ?, ?, ?, ?)', [site, url, word, memo, folder_id]);
       callback();
     });
   }
-
 }
