949551d1f8b14b283e6a2ec647ed27bcd290fae8
nemu-ism <kerokeromirai@gmail.com>
2019-08-09 16:53:57 +0900

    popupEdit.jsのDAOメソッドをprototypeに記述

--- js/popupEdit.js
+++ js/popupEdit.js
@@ -1,41 +1,41 @@
-$(function(){
+$(function() {
   var dao = new Dao();
   // サイト名を取得
   chrome.tabs.getSelected(null, function(tab) {
   });
 
   // 編集登録
-  $('#Bt_Edit').on('click',function(){
+  $('#Bt_Edit').on('click',function() {
       var id = getParam(0);
       var site = $('#input_site').val().trim();
       var url = getParam(2).trim();
       var word = $('input#search_word').val().trim();
       var memo = $('textarea#input_memo').val().trim();
       var folder_id = getParam(5);
-      dao.update(id, site, url, word, memo, function(){
+      dao.update(id, site, url, word, memo, function() {
         window.location.href = '/popup.html' + "?folder_id=" + folder_id;
       });
   });
 
   //削除
-  $('#Bt_Remove').on('click', function(){
+  $('#Bt_Remove').on('click', function() {
     const id = getParam(0);
     var folder_id = getParam(5);
     dao.remove(id, function() {
       window.location.href = '/popup.html' + "?folder_id=" + folder_id;
     });
-  })
+  });
 
   // リストに戻る
-  $('#Bt_Cancel').on('click', function(){
+  $('#Bt_Cancel').on('click', function() {
   var folder_id = getParam(5);
     window.location.href = '/popup.html' + "?folder_id=" + folder_id;
-  })
+  });
 
   // close
-  $('.close').on('click', function(){
+  $('.close').on('click', function() {
     window.close();
-  })
+  });
 
   $('#input_site').val(getParam(1));
   $('.memo').val(getParam(3));
@@ -78,23 +78,21 @@ var Dao = function(){
   var version = '1.0';
   var description = 'Web SQL Database';
   var size = 5 * 1024 * 1024;
-  var db = openDatabase(name, version, description, size);
+  this.db = openDatabase(name, version, description, size);
+}
 
-  // 編集保存
-  this.update = function(id, site, url, word, memo, callback){
-    db.transaction(function (tx){
+Dao.prototype = {
+  update: function(id, site, url, word, memo, callback) {
+    this.db.transaction(function(tx) {
       tx.executeSql('update bookmarks SET name=?, url=?, search_word=?, memo=? WHERE id=?', [site, url, word, memo,id]);
       callback();
     });
-  }
+  },
 
-  // 削除
-  this.remove = function(id, callback){
-    db.transaction(function (tx){
+  remove: function(id, callback) {
+    this.db.transaction(function(tx) {
       tx.executeSql('delete from bookmarks where id = ?', [id]);
       callback();
     });
   }
-
-
 }
